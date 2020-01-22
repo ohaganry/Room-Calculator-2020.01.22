bb50a12f9bbea1ea46059f763a7c879147bbfcf6
ohaganry <hermits.nature@gmail.com>
2020-01-22 10:00:54 -0500

    First push for Room Calculator

---
+++ rclib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace rclib
+{
+    public class Class1
+    {
+    }
+}
---
+++ roomcalc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace roomcalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string proceed = "y";
+           while(proceed == "y")
+            {
+                Console.Write("What is the length of the room in ft? ");
+                float length = float.Parse(Console.ReadLine());
+                Console.Write("What is the width of the room in ft? ");
+                float width = float.Parse(Console.ReadLine());
+                Console.Write("What is the height of the room in ft? ");
+                float height = float.Parse(Console.ReadLine());
+
+                float perimeter = (length * 2) + (width * 2);
+                float area = (length * width);
+                float volume = (length * width * height);
+
+                Console.WriteLine($"Perimeter: {perimeter} ft. \n Area: {area} sq.ft. \n Volume: {volume} cu.ft.");
+                Console.WriteLine("Would you like to measure another room? (y/n)");
+                proceed = Console.ReadLine();
+            }
+        }
+    }
+}
